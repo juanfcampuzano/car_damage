e0357105742d4a081343ad86134bd5f7b83bcd62
Juan Campuzano <juanfecazuluaga@gmail.com>
2024-03-02 15:59:30 -0500

    Primer commit

---
+++ main.py
@@ -0,0 +1,90 @@
+import skimage.io as io
+from detectron2 import model_zoo
+from detectron2.config import get_cfg
+from detectron2.engine import DefaultPredictor
+import os
+import boto3
+from scipy.spatial import distance
+from fastapi import FastAPI, File, UploadFile, HTTPException
+import numpy as np
+from PIL import Image
+from io import BytesIO
+from pydantic import BaseModel
+
+app = FastAPI()
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
+
+if not all([AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_BUCKET_NAME]):
+    raise ValueError("Faltan las variables de entorno AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY y AWS_BUCKET_NAME")
+
+s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+@app.on_event("startup")
+async def startup_event():
+    try:
+        os.makedirs("models", exist_ok=True)
+        s3.download_file(AWS_BUCKET_NAME, 'damage_segmentation_model.pth', 'models/damage_segmentation_model.pth')
+        s3.download_file(AWS_BUCKET_NAME, 'part_segmentation_model.pth', 'models/part_segmentation_model.pth')
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+def detect_damage_part(damage_dict, parts_dict):
+  try:
+    max_distance = 10e9
+    assert len(damage_dict)>0, "AssertError: damage_dict should have atleast one damage"
+    assert len(parts_dict)>0, "AssertError: parts_dict should have atleast one part"
+    max_distance_dict = dict(zip(damage_dict.keys(),[max_distance]*len(damage_dict)))
+    part_name = dict(zip(damage_dict.keys(),['']*len(damage_dict)))
+
+    for y in parts_dict.keys():
+        for x in damage_dict.keys():
+          dis = distance.euclidean(damage_dict[x], parts_dict[y])
+          if dis < max_distance_dict[x]:
+            part_name[x] = y.rsplit('_',1)[0]
+
+    return list(set(part_name.values()))
+  except Exception as e:
+    print(e)
+
+
+damage_class_map= {0:'damage'}
+parts_class_map={0:'headlamp',1:'rear_bumper', 2:'door', 3:'hood', 4: 'front_bumper'}
+
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  # only has one class (damage) + 1
+cfg.MODEL.RETINANET.NUM_CLASSES = 2 # only has one class (damage) + 1
+cfg.MODEL.WEIGHTS = os.path.join("models/damage_segmentation_model.pth")
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7 
+cfg['MODEL']['DEVICE']='cpu'#or cpu
+damage_predictor = DefaultPredictor(cfg)
+
+
+cfg_mul = get_cfg()
+cfg_mul.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg_mul.MODEL.ROI_HEADS.NUM_CLASSES = 6  # only has five classes (headlamp,hood,rear_bumper,front_bumper_door) + 1
+cfg_mul.MODEL.RETINANET.NUM_CLASSES = 6 # only has five classes (headlamp,hood,rear_bumper,front_bumper_door) + 1
+cfg_mul.MODEL.WEIGHTS = os.path.join("models/part_segmentation_model.pth")
+cfg_mul.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7 
+cfg_mul['MODEL']['DEVICE']='cpu' #or cpu
+part_predictor = DefaultPredictor(cfg_mul)
+
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    im = np.array(Image.open(BytesIO(await file.read())))
+
+    damage_outputs = damage_predictor(im)
+    parts_outputs = part_predictor(im)
+
+    damage_prediction_classes = [ damage_class_map[el] + "_" + str(indx) for indx,el in enumerate(damage_outputs["instances"].pred_classes.tolist())]
+    damage_polygon_centers = damage_outputs["instances"].pred_boxes.get_centers().tolist()
+    damage_dict = dict(zip(damage_prediction_classes,damage_polygon_centers))
+    parts_prediction_classes = [ parts_class_map[el] + "_" + str(indx) for indx,el in enumerate(parts_outputs["instances"].pred_classes.tolist())]
+    parts_polygon_centers =  parts_outputs["instances"].pred_boxes.get_centers().tolist()
+    parts_polygon_centers_filtered = list(filter(lambda x: x[0] < 800 and x[1] < 800, parts_polygon_centers))
+    parts_dict = dict(zip(parts_prediction_classes,parts_polygon_centers_filtered))
+    return "Damaged Parts: " + str(detect_damage_part(damage_dict,parts_dict))
